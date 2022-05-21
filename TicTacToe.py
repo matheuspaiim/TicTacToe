ac5943b94bb2794500505e219e54eb780495567f
Matheus Paim <65717016+matheuspaiim@users.noreply.github.com>
2022-05-20 21:31:26 -0300

    organizing code

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,41 +3,33 @@ import random
 import tkinter as tk
 from tkinter import ttk
 
+# GAME OPERATION
 
-def next_turn(row, column):
 
-    global player
+def new_game():
+    global player 
 
-    if buttons[row][column]['text'] == "" and check_winner() is False:
+    label.config(text="Vez de "+player)
 
-        if player == players[0]:
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="", bg="#F0F0F0")
 
-            buttons[row][column]['text'] = player
 
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=("Vez de "+players[1]))
+def empty_spaces():
 
-            elif check_winner() is True:
-                label.config(text=(players[0]+" Venceu"))
+    spaces = 9
 
-            elif check_winner() == "Empate":
-                label.config(text="Empate!")
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text'] != "":
+                spaces -= 1
 
-        else:
-
-            buttons[row][column]['text'] = player
-
-            if check_winner() is False:
-                player = players[0]
-                label.config(text=("Vez de "+players[0]))
-
-            elif check_winner() is True:
-                label.config(text=(players[1]+" Venceu"))
-
-            elif check_winner() == "Empate":
-                label.config(text="Empate!")
-
+    if spaces == 0:
+        return False
+    else:
+        return True
+    
 
 def check_winner():
 
@@ -78,38 +70,62 @@ def check_winner():
         return False
 
 
-def empty_spaces():
+# DIFFICULTIES
 
-    spaces = 9
+def human_player(row, column):
 
-    for row in range(3):
-        for column in range(3):
-            if buttons[row][column]['text'] != "":
-                spaces -= 2
+    global player
 
-    if spaces == 0:
-        return False
-    else:
-        return True
+    if buttons[row][column]['text'] == "" and check_winner() is False:
+
+        if player == players[0]:
+
+            buttons[row][column]['text'] = player 
+
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=("Vez de "+players[1]))
+
+            elif check_winner() is True:
+                label.config(text=(players[0]+" Venceu"))
+
+            elif check_winner() == "Empate":
+                label.config(text="Empate!")
+
+        else:
+
+            buttons[row][column]['text'] = player
+
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=("Vez de "+players[0]))
+
+            elif check_winner() is True:
+                label.config(text=(players[1]+" Venceu"))
+
+            elif check_winner() == "Empate":
+                label.config(text="Empate!")
+
+def easy():
+    pass
+
+def medium():
+    pass
+
+def hard():
+    pass
 
 
-def new_game():
-    global player 
-
-    label.config(text="Vez de "+player)
-
-    for row in range(3):
-        for column in range(3):
-            buttons[row][column].config(text="", bg="#F0F0F0")
-
+# MAINFRAME
 
 window = Tk()
 window.title("TicTacToe")
 players = ["X", "O"]
-player = "X"
+player = random.choice(players)
 buttons = [[0, 0, 0],
            [0, 0, 0],
            [0, 0, 0]]
+
 
 label = Label(text="Vez de "+player, font=('consolas', 40))
 label.grid(row=1, pady=60)
@@ -118,38 +134,45 @@ reset_button = Button(window, text="restart", font=(
     'consolas', 10), command=new_game)
 reset_button.place(x=50, y=137)
 
+frame = Frame(window)
+frame.grid()
+
+# DROPDOWN
 
 tkvar = StringVar(window)
-choices = ['Fácil','Médio','Difícil', 'Jogar contra um amigo']
-tkvar.set('Fácil') # set the default option
-
+choices = ["Fácil", "Médio", "Difícil", "Jogar contra um amigo"]
+tkvar.set(choices[0]) # SET THE DEFAULT OPTION
 popupMenu = OptionMenu(window, tkvar, *choices)
 Label(window, text="Escolha a dificuldade:").place(x=140, y=140)
 popupMenu.place(x=280, y=135)
 
-def change_dropdown(*args):
-    print( tkvar.get() )
+def change_dropdown(*choices):
+    
+    choices = tkvar.get()
+    
+    if choices == "Fácil":
+        easy()
+    elif choices == "Médio":
+        medium()
+    elif choices == "Difícil":
+        hard()
+    elif choices == "Jogar contra um amigo":
+        human_player(row, column)
+        new_game()
+           
     
 tkvar.trace('w', change_dropdown)
+  
+Label(window, text="Escolha o jogador:").place(x=100, y=30)  
         
+# CHOOSE O BUTTON 
     
 def clicked_o():  
     
     if player == players[0]:
         label.config(text=("Vez de "+players[1]))
-        next_turn(row, column)
+        human_player(row, column)
         new_game()
-
-        
-def clicked_x():
-    
-    if player == players[1]:
-        label.config(text=("Vez de "+players[0]))
-        next_turn(row, column)
-        new_game()
-
-    
-Label(window, text="Escolha o jogador:").place(x=100, y=30)
 
 icon_o = tk.PhotoImage(file='./assets/o.png')
 button_o = ttk.Button(
@@ -159,8 +182,16 @@ button_o = ttk.Button(
     
 )
 
-
 button_o.place(x=250, y=15)
+
+# CHOOSE X BUTTON
+        
+def clicked_x():
+    
+    if player == players[1]:
+        label.config(text=("Vez de "+players[0]))
+        human_player(row, column)
+        new_game()
 
 icon_x = tk.PhotoImage(file='./assets/x.png')
 button_x = ttk.Button(
@@ -172,13 +203,12 @@ button_x = ttk.Button(
 button_x.place(x=330, y=15)
 
 
-frame = Frame(window)
-frame.grid()
+# BOARD (BUTTONS)
 
 for row in range(3):
     for column in range(3):
         buttons[row][column] = Button(frame, text="", font=('consolas', 40), width=5, height=2,
-                                      command=lambda row=row, column=column: next_turn(row, column))
+                                      command=lambda row=row, column=column: human_player(row, column))
         buttons[row][column].grid(row=row, column=column)
 
 window.mainloop()
